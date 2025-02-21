0f3416f9ad9ea7438fcdee3cd51b858a802e60a1
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-21 12:05:00 +0530

    initialize React + TypeScript + Vite project with Tailwind CSS and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  
+
+  return (
+    <div className=" bg-slate-700 min-w-dvw min-h-[100vh] items-center flex flex-col justify-center">
+      <h2>Token Input</h2>
+
+    </div>
+  );
+}
+
+export default App;
